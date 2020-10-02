1746c2ab7fb080ff6846389df3eb375963ccdd43
胜 南 <nslove442024898@live.com>
2020-10-02 10:21:56 +0800

    添加项目文件。

---
+++ comDll/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace comDll
+{
+    public class Mycls
+    {
+        public double MyPlus(double a, double b)
+        {
+            return a + b + 0.5;
+        }
+    }
+}
